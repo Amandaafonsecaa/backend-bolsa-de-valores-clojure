65b8dffbce6d0fd2cc12f6ee03c6d57efbf45432
Amandaafonsecaa <amandafonsecarod09@gmail.com>
2025-11-26 18:50:09 -0300

    feat: routes transacao nao testadas

---
+++ src/bolsa_de_valores/routes.clj
@@ -0,0 +1,11 @@
+(ns bolsa-de-valores.routes
+  (:require [compojure.core :refer :all]
+            [bolsa-de-valores.controllers.transacao_controller :as transacao]))
+
+(defroutes app-routes
+
+  (POST "/transacoes/compra"  request (transacao/comprar request))
+  (POST "/transacoes/venda"   request (transacao/vender request))
+  (GET  "/transacoes"         request (transacao/extrato request))
+
+  (route/not-found "Rota não encontrada"))
